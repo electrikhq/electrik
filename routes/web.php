bc404aab18f21f86d4d986d78a504c81d8dc4322
neerajsohal <hello@neerajkumar.name>
2023-01-12 11:00:09 +0530

    resolved #19

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web'])->group(function($route) {
@@ -97,9 +99,13 @@ Route::middleware(['web'])->group(function($route) {
 
 	});
 
-	$route->get('logout', function() {
+	$route->get('logout', function(Request $request) {
+		
+		Auth::guard('web')->logout();
+		$request->session()->invalidate();
+		$request->session()->regenerateToken();
+		return redirect('/');
 
 	})->name('logout');
-
 
 });
